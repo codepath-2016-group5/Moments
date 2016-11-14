9d68541d295b908472363e568ebe8f92af1a871b
Mahesh Dathrika <mdathrika@ebay.com>
2016-11-14 11:30:30 -0800

    Added Facebook & Maps Integration

--- app/src/main/java/com/codepath/apps/findmate/models/User.java
+++ app/src/main/java/com/codepath/apps/findmate/models/User.java
@@ -1,19 +1,31 @@
 package com.codepath.apps.findmate.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+
+import java.util.List;
 
 /**
  * Created by sdevired on 11/9/16.
  */
 @ParseClassName("User")
-public class User extends ParseObject {
+public class User extends ParseObject implements Parcelable {
 
 
     public static final String ID_KEY = "id";
 
     public static final String NAME_KEY = "name";
 
+    public static final String EMAIL = "email";
+
+    public static final String FULL_NAME = "full_name";
+
+    public static final String FB_ID = "fb_id";
+
+    public static final String FRIENDS ="friends";
 
 
     public String getId() {
@@ -46,6 +58,63 @@ public class User extends ParseObject {
 
     }
 
+    public String getEmail() {
+        return getString(EMAIL);
+    }
+
+    public void setEmail(String email) {
+        put(EMAIL, email);
+    }
+
+    public String getFullName() {
+        return getString(FULL_NAME);
+    }
+
+    public void setFullName(String fullName) {
+        put(FULL_NAME, fullName);
+    }
+
+    public String getFbId() {
+        return getString(FB_ID);
+    }
+
+    public void setFbId(String fbId) {
+        put(FB_ID, fbId);
+    }
+
+    public List<User> getFriends() {
+        return getList(FRIENDS);
+    }
+
+    public void setFriends(List<User> friends) {
+        put(FRIENDS, friends);
+    }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    public User() {
+    }
+
+    protected User(Parcel in) {
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }

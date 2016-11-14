f336a81f0dd17b0424d44fa2b2fa44d3c01463f7
Anirudh Padmarao <ani@pexlabs.com>
2016-11-13 20:58:07 -0800

    Add screen to view events

---
+++ app/src/main/java/com/codepath/apps/findmate/EventsAdapter.java
@@ -0,0 +1,76 @@
+package com.codepath.apps.findmate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.findmate.models.Event;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Event> events;
+
+    public EventsAdapter(Context context, List<Event> events) {
+        this.context = context;
+        this.events = events;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View eventView = inflater.inflate(R.layout.item_event, parent, false);
+        return new ViewHolder(eventView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Event event = events.get(position);
+
+        if (event.getPictureUrl() != null) {
+            Picasso.with(context).load(event.getPictureUrl()).into(holder.ivEventImage);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(event.getStart());
+        holder.tvEventDay.setText(Integer.toString(calendar.get(Calendar.DAY_OF_MONTH)));
+        holder.tvEventMonth.setText(new SimpleDateFormat("MMM").format(calendar.getTime()));
+
+        holder.tvEventName.setText(event.getName());
+        holder.tvEventDatePlace.setText(event.getDetails());
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivEventImage;
+        public TextView tvEventDay;
+        public TextView tvEventMonth;
+        public TextView tvEventName;
+        public TextView tvEventDatePlace;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            ivEventImage = (ImageView) itemView.findViewById(R.id.ivEventImage);
+            tvEventDay = (TextView) itemView.findViewById(R.id.tvEventDay);
+            tvEventMonth = (TextView) itemView.findViewById(R.id.tvEventMonth);
+            tvEventName = (TextView) itemView.findViewById(R.id.tvEventName);
+            tvEventDatePlace = (TextView) itemView.findViewById(R.id.tvEventDatePlace);
+        }
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/findmate/EventsFragment.java
@@ -0,0 +1,63 @@
+package com.codepath.apps.findmate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.findmate.models.Event;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class EventsFragment extends Fragment {
+
+    private List<Event> events;;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        events = createEventsList();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvEvents = (RecyclerView) view.findViewById(R.id.rvEvents);
+        EventsAdapter adapter = new EventsAdapter(getActivity(), events);
+        rvEvents.setAdapter(adapter);
+        rvEvents.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private static List<Event> createEventsList() {
+        Event movie = new Event()
+                .setName("Doctor Strange Viewing")
+                .setDetails("Dr. Stephen Strange's (Benedict Cumberbatch) life changes after a car " +
+                        "accident robs him of the use of his hands. When traditional medicine fails " +
+                        "him, he looks for healing, and hope, in a mysterious enclave. He quickly " +
+                        "learns that the enclave is at the front line of a battle against unseen " +
+                        "dark forces bent on destroying reality. Before long, Strange is forced " +
+                        "to choose between his life of fortune and status or leave it all behind " +
+                        "to defend the world as the most powerful sorcerer in existence.")
+                .setPictureUrl("https://i.annihil.us/u/prod/marvel/i/mg/b/d0/568ad0a32ead4.jpg")
+                .setStart(new Date());
+        Event sport = new Event()
+                .setName("Tennis match")
+                .setDetails("Play tennis doubles with group")
+                .setStart(new Date());
+
+        List<Event> events = new ArrayList<>();
+        events.add(movie);
+        events.add(sport);
+        return events;
+    }
+}
